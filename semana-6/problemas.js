989d7ca4de08c1e48dbf06b348bd189264afe340
linder3hs <linderhassinger00@gmail.com>
2024-03-06 22:09:47 -0500

    ➕ feat: problems

--- semana-6/problemas.js
+++ semana-6/problemas.js
@@ -20,3 +20,32 @@ function invertirCadena(texto) {
 invertirCadena(100); // 001
 invertirCadena(true);
 invertirCadena("hola mundo");
+
+/*
+Contar la cantidad de vocales en una cadena:
+
+Crea una función que tome una cadena(string) como entrada y devuelva la cantidad de vocales que contiene. Puedes considerar tanto las vocales en mayúsculas como en minúsculas.
+
+hola => 2
+celular => 3
+telefono => 4
+*/
+
+function contarVocales(texto) {
+  if (typeof texto !== "string") return "No es un texto";
+
+  const vocales = "aeiou".split("");
+  const textoEnMiniscula = texto.toLowerCase();
+  let contador = 0;
+
+  for (let i = 0; i < textoEnMiniscula.length; i++) {
+    if (vocales.includes(textoEnMiniscula[i])) {
+      contador++;
+    }
+  }
+  // template string
+  return `La cantidad de vocales son: ${contador}`;
+}
+
+contarVocales("telefono");
+contarVocales("celular");
