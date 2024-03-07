24a120f0b4c0aa931de245099ecebf5204726b45
linder3hs <linderhassinger00@gmail.com>
2024-03-06 20:14:52 -0500

    ➕ feat: intro js

---
+++ semana-6/intro.js
@@ -0,0 +1,74 @@
+//Cuantas formas de crear una VARIABLE
+
+// mutable = el valor o tipo puede cambiar
+// inmutable = el valor o tipo no puede cambiar
+
+// MUTABLE
+/* var
+   - La forma antigua de declarar variables
+   - scope es mucho mayor
+   - ocupa mas espacio en memoria
+*/
+var nombre = "Linder";
+var nombre = 10;
+
+console.log("usando var", nombre);
+/*
+  let
+  - La forma moderna de declarar variales
+  - El scope es en base al ambito donde se crear
+  - tiene una mejor optimización en memoria
+*/
+
+// creamos la variable con valor 10
+let edad = 10;
+// verificar que la variable edad exista, le reasigna un nuevo valor y un nuevo tipo
+edad = "Linder";
+
+// cuando creamos una variable sin prefijo JS lo asume como si se usara var
+direccion = "Av siempre viva";
+
+// INMUTABLE
+const pi = Math.PI;
+const fechaDeNacimiento = "1900-01-01";
+const dni = "88888888";
+const tipoDeSangre = "O+";
+console.log(pi);
+
+// tipos de datos
+/*
+string
+number
+bool
+undefined
+object
+null
+symbol
+BigInt
+*/
+
+// Listas (arrays) / Fechas /
+
+// string
+let apellido = "Hassinger";
+console.log(typeof apellido);
+// number
+let altura = 1.78;
+console.log(typeof altura);
+// bool
+let esMayorDeEdad = true; // false
+let es_mayor_de_edad = true; // no recomendable
+let esMDEdad = false; // mala practica
+console.log(typeof esMayorDeEdad);
+
+// undefined
+let numeroDeCelular = undefined;
+console.log(typeof numeroDeCelular);
+
+// null // se olvidaron de null
+let variable = null;
+console.log(typeof variable);
+
+let suma = 0;
+console.log(null + 10);
+console.log(0 + 10);
