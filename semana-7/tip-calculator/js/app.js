6c58d93b92cb5c89cffc5fe70e780721271c47aa
Linder Hassinger <linder@peppermint.pe>
2024-03-13 22:43:12 -0500

    ➕ feat: add html form JS

--- semana-7/tip-calculator/js/app.js
+++ semana-7/tip-calculator/js/app.js
@@ -1,5 +1,34 @@
 const containerBill = document.querySelector("#contailer-bill");
+const containerPercentages = document.querySelector("#container-percentage");
 const inputBill = document.querySelector("#input-bill");
+
+// array con los valores de los botons
+const percentages = [
+  {
+    type: "button",
+    value: "5%",
+  },
+  {
+    type: "button",
+    value: "10%",
+  },
+  {
+    type: "button",
+    value: "20%",
+  },
+  {
+    type: "button",
+    value: "25%",
+  },
+  {
+    type: "button",
+    value: "50%",
+  },
+  {
+    type: "input",
+    value: "0",
+  },
+];
 
 // click
 containerBill.onclick = function () {
@@ -19,3 +48,27 @@ inputBill.onkeyup = function (event) {
     this.value = inputValue.slice(0, -1);
   }
 };
+
+function renderButton(text) {
+  return `<button>${text}</button>`;
+}
+
+function renderInput() {
+  return `<input
+            type="text"
+            class="outline-none p-2 bg-[#F3F8FB] rounded-md"
+            placeholder="Custom"
+          />`;
+}
+
+// paso 1 es limpiar el contenido del container
+containerPercentages.innerHTML = "";
+
+percentages.forEach(function (percentage) {
+  const html =
+    percentage.type === "button"
+      ? renderButton(percentage.value)
+      : renderInput();
+
+  containerPercentages.innerHTML += html;
+});
