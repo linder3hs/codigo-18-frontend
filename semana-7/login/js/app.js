7a476e497a306071e7ee13bb77ee50495fe22920
Linder Hassinger <linder@peppermint.pe>
2024-03-15 20:19:42 -0500

    ➕ feat: info from form

---
+++ semana-7/login/js/app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form");
+
+/**
+ * Como hacemos para poder capturar el evento submit del form?
+ *
+ * Sencillo, solo debemos usar el evento onsubmit, ahora recordemos
+ * que este evento es solo de los formularios
+ *
+ * Ejemplo
+ *
+ * form.onsubmit = function (event) {
+ *  ...
+ * }
+ */
+
+form.onsubmit = function (event) {
+  /**
+   * Los formulario tienen la tendencia recargar la página
+   * para evitar que el evento onSubmit recargue la pagina
+   * vamos usar la funcion preventDefault() esta funcion es parte
+   * del evento que recibimos en nuestra funcion
+   */
+  event.preventDefault();
+  /**
+   * Debemos saber que toda la informacion del formulario esta en event
+   * console.log(event)
+   */
+  console.log(event);
+};
---
+++ semana-7/login/js/users.js
@@ -0,0 +1,26 @@
+const users = [
+  {
+    id: 1,
+    name: "Linder",
+    lastname: "Hassinger",
+    email: "linder@gmail.com",
+    password: "linder340",
+    document_number: "88888",
+  },
+  {
+    id: 2,
+    name: "Pepito",
+    lastname: "Perez",
+    email: "pepe@gmail.com",
+    password: "pepe123",
+    document_number: "777777",
+  },
+  {
+    id: 3,
+    name: "Juan",
+    lastname: "Zapata",
+    email: "juan@gmail.com",
+    password: "juan123",
+    document_number: "1111111",
+  },
+];
