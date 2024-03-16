3abaf338c28cbe71a71caa91bfe24e7020c3a5d5
Linder Hassinger <linder@peppermint.pe>
2024-03-15 23:02:21 -0500

    ➕ feat: logount

--- semana-7/login/js/home.js
+++ semana-7/login/js/home.js
@@ -18,3 +18,29 @@ const email = document.querySelector("#email");
 const userObj = JSON.parse(user);
 fullName.textContent = `${userObj.name} ${userObj.lastname}`;
 email.textContent = userObj.email;
+
+// Button para hacer logout
+const btnLogout = document.querySelector("#btn-logout");
+
+btnLogout.onclick = function () {
+  // Paso1: Vamos a mostrar un alerta para que el usuario confirme
+  Swal.fire({
+    title: "Esta seguro?",
+    text: "de cerrar sesión en Codigo Tecsup?",
+    icon: "warning",
+    showCancelButton: true,
+    cancelButtonText: "Cancelar",
+    confirmButtonText: "Si, estoy seguro",
+  }).then(function (value) {
+    if (!value.isConfirmed) {
+      return;
+    }
+    // Paso1: Limpiamos localStorage
+    // solo 1 elemento basado en el key
+    // localStorage.removeItem("user");
+    // elimina todo lo que este en localStorage
+    localStorage.clear();
+
+    location.href = "http://127.0.0.1:5500/semana-7/login/";
+  });
+};
