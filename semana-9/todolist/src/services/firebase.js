eed289745d237dd7a29d729627b7af295e3770a3
Linder Hassinger <linder@peppermint.pe>
2024-04-17 23:07:11 -0500

    ➕ feat: firebase login

--- semana-9/todolist/src/services/firebase.js
+++ semana-9/todolist/src/services/firebase.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBlWDKtkbgcHds1vyO13xev36wCI-NbCaM",
@@ -22,9 +26,26 @@ export async function createUser(email, password) {
       password
     );
 
-    console.log("success", authentication.user);
+    return authentication.user;
   } catch (error) {
     console.log(error.code);
     console.log(error.message);
+    return null;
+  }
+}
+
+export async function signIn(email, password) {
+  try {
+    const authentication = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    return authentication.user;
+  } catch (error) {
+    console.log(error.code);
+    console.log(error.message);
+    return null;
   }
 }
