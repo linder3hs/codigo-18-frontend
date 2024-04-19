eb205d301bdbb68298a10e1a7866bddb48fd7063
Linder Hassinger <linder@peppermint.pe>
2024-04-18 22:54:16 -0500

    ➕ feat: isLoading

--- semana-9/todolist/src/pages/SignUp/index.jsx
+++ semana-9/todolist/src/pages/SignUp/index.jsx
@@ -3,6 +3,8 @@ import { TextField } from "../../components";
 import { createUser, storeFile, updateUser } from "../../services/firebase";
 
 export default function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const [values, setValues] = useState({
     fullName: "",
     email: "",
@@ -22,6 +24,7 @@ export default function SignUp() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
     const url = await storeFile(event.target[4].files[0]);
 
     // create al usuario en firebase
@@ -29,6 +32,7 @@ export default function SignUp() {
     await createUser(values.email, values.password);
     // actualizamos al usuario que hemos creado con su nombre y su foto de perfil
     await updateUser(values.fullName, url);
+    setIsLoading(false);
   };
 
   return (
@@ -81,29 +85,31 @@ export default function SignUp() {
             <div className="my-5">
               <button
                 type="submit"
-                className="border border-green-400 text-black font-bold w-full px-3 py-2 rounded-md
+                className="border flex items-center justify-center border-green-400 text-black font-bold w-full px-3 py-2 rounded-md
               hover:bg-green-400 hover:text-white transition-all ease-in-out duration-300"
               >
-                <svg
-                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-black"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
+                {isLoading && (
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-black"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                )}
                 Registrarme
               </button>
             </div>
