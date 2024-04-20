f1fde12aee9b508f510fdefaac9f194dce232be5
Linder Hassinger <linder@peppermint.pe>
2024-04-19 22:46:24 -0500

    ➕ feat: main layout

---
+++ semana-9/todolist/src/layouts/MainLayout/index.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { getCurrentUser } from "../../services/auth";
+import { useNavigate } from "react-router-dom";
+
+export default function MainLayout() {
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      const currentUser = await getCurrentUser();
+      setUser(currentUser);
+    } catch (error) {
+      navigate("/login");
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <div>
+      <nav className="bg-white py-2 px-12">
+        <div className="flex items-center gap-5 justify-end">
+          <div>
+            <p className="font-bold">{user.displayName}</p>
+            <p className="text-xs font-light">{user.email}</p>
+          </div>
+          <img
+            src={user.photoURL}
+            className="rounded-full w-[40px] h-[40px] object-cover"
+            alt=""
+          />
+        </div>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
--- semana-9/todolist/src/router/index.jsx
+++ semana-9/todolist/src/router/index.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage, LoginPage, SignUpPage } from "../pages";
-import { AuthLayout } from "../layouts";
+import { AuthLayout, MainLayout } from "../layouts";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
         <Route element={<AuthLayout />}>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/sign-up" element={<SignUpPage />} />
